da04bc1eef1731b569e6fed8fd4532c505790895
JosR <josueramos17@hotmail.com>
2023-09-24 10:57:46 -0500

    refactor: delete unused elements and modified styles

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import NewCommunity from "@/components/pages/community/new-community.vue";
-import Subscription from "@/components/views/subscription.component.vue";
 import NewActivity from "@/components/pages/activity/new-activity.vue";
 import LoginComponent from "@/components/pages/user/login.component.vue";
 import SignupComponent from "@/components/pages/user/signup.component.vue";
@@ -17,7 +16,6 @@ const routes = [
     { path: '/new-activity', component: NewActivity, meta: {requiresAuth: true}},
     { path: '/list-activities', component: ListActivities, meta: {requiresAuth: true} },
     { path: '/new-community', component: NewCommunity, meta: {requiresAuth: true} },
-    { path: '/subscription', component: Subscription },
     { path: '/:catchAll(.*)', component: NotFoundComponent },
 ]
 
